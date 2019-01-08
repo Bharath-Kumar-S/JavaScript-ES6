e9cc2d680c2798f6140fa7b193e19334f72be8b8
Bharath Kumar <bharath.kum.s@hpe.com>
2019-01-08 12:22:57 +0530

    event loop

--- async_callbacks.js
+++ async_callbacks.js
@@ -1,7 +1,18 @@
+// console.log('Sync code')
+
+// setTimeout(_ => console.log('Timeout'),0)
+
+// Promise.resolve().then(_ => console.log('Promise'))
+
+// console.log('sync code')
+
+
 console.log('Sync code')
 
-setTimeout(_ => console.log('Timeout'),0)
+new Promise((resolve, reject) => {
+    setTimeout(_ => resolve( console.log('Promise')), 0)
+})
 
-Promise.resolve().then(_ => console.log('Promise'))
+setTimeout(_ => console.log('Timeout'), 0)
 
 console.log('sync code')
